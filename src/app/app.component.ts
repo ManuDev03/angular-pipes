4fe62a314a8aa725efb37dccf9f1efe3cd6a11b7
ManuDev03 <=manuprajan03@gmail.com>
2021-11-07 22:24:13 +0530

    formating dates using DatePipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'pipes';
   name : string;
+  date : string;
+
   onNameChange(value:string){
     this.name = value
+  }
+
+  onDateChange(value:string){
+    this.date = value
   }
 }
